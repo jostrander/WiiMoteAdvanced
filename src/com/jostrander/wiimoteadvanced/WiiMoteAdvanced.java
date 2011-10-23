090a85c410f7ad2f6f911e72c33aad568ebb228f
jostrander <jesse.ostrander@gmail.com>
2011-10-23 14:37:26 -0400

    Implemented a Default Listener Strictly for status updates. Started GUI for display chooser.

--- src/com/jostrander/wiimoteadvanced/WiiMoteAdvanced.java
+++ src/com/jostrander/wiimoteadvanced/WiiMoteAdvanced.java
@@ -1,6 +1,18 @@
 package com.jostrander.wiimoteadvanced;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class WiiMoteAdvanced {
+	private static JDialog screens[];
+
 	public static void main(String[] args) {
 		/*
 		 * We need to know a few things before we can run the program. 1. How
@@ -10,7 +22,65 @@ public class WiiMoteAdvanced {
 		 */
 		System.loadLibrary("wiiuse");
 		System.loadLibrary("wiiusej");
+		// Screen and preferences here.
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gs = ge.getScreenDevices();
+		if (gs.length < 1)
+			System.exit(0);
+		if (gs.length == 1) {
+			// Only one display, so can only be shown on the main display...so
+			// do that here.
+			int height = gs[0].getDisplayMode().getHeight();
+			int width = gs[0].getDisplayMode().getWidth();
+			String idstring = gs[0].getIDstring();
+			System.out.println("1 Graphics Device Found.");
+			System.out.println(width + "x" + height + " " + idstring);
+
+		} else {
+			// check prefs here to see if anything has changed, if not select
+			// previous setup.
+			// if changed, prompt again.
+			for (int i = 0; i < gs.length; i++) {
+				Rectangle bounds = gs[i].getDefaultConfiguration().getBounds();
+				String idstring = gs[i].getIDstring();
+				showScreenHint(i, bounds, idstring.replace('\\', ' '));
+			}
+		}
 		GUI.init();
-		WiimoteManager.init();
+		WiimoteManager.connect();
+		Runnable r = new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					WiimoteManager.requestUpdate();
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
+			}
+			
+		};
+	}
+
+	private static void showScreenHint(int id, Rectangle r, String name) {
+		try {
+			screens[id] = new JDialog();
+			JDialog hinter = screens[id];
+			JLabel label = new JLabel("Device " + id
+					+ "\nClick on a screen to select it.");
+			label.setForeground(new Color(255, 255, 255));
+			label.setHorizontalAlignment(SwingConstants.CENTER);
+			hinter.setUndecorated(true);
+			hinter.setAlwaysOnTop(true);
+			hinter.setBackground(new Color(0, 0, 0, 100));
+			hinter.setLocation(r.x, r.y);
+			hinter.setSize(new Dimension(r.width, r.height));
+			hinter.add(label);
+			hinter.setVisible(true);
+		} catch (NullPointerException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
--- src/com/jostrander/wiimoteadvanced/WiimoteManager.java
+++ src/com/jostrander/wiimoteadvanced/WiimoteManager.java
@@ -1,21 +1,31 @@
 package com.jostrander.wiimoteadvanced;
 
+import com.jostrander.wiimoteadvanced.listeners.DefaultListener;
+
 import wiiusej.WiiUseApiManager;
 import wiiusej.Wiimote;
 
 
 public class WiimoteManager {
 	public static Boolean isConnected = false;
-	public static Boolean rumbleOnConnect = false;
-	public static void init() {
+	public static Boolean rumbleOnConnect = true;
+	private static Wiimote wiimote;
+	public static void connect() {
 		if (!isConnected) {
 			Wiimote[] wiimotes = WiiUseApiManager.getWiimotes(1, rumbleOnConnect);
-			Wiimote wiimote;
 			try {
 				wiimote = wiimotes[0];
+				isConnected = true;
+				GUI.connectedtf.setText("Wiimote "+wiimote.getId());
+				wiimote.addWiiMoteEventListeners(new DefaultListener());
 			} catch (ArrayIndexOutOfBoundsException e) {
 				System.out.println("No Wiimotes Found!");
 			}
+		} else {
+			System.out.println("WiiMotes already Connected!");
 		}
+	}
+	public static void requestUpdate() {
+		wiimote.getStatus();
 	}
 }
